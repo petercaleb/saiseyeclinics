e8370871392816314d461bb920726e5cf77a97b8
petercaleb <petermwasagua@gmail.com>
2025-04-16 06:37:40 +0300

    Made changes to duplicate table migrations

--- main/database/migrations/2025_04_14_222944_create_lens_prescriptions_1_table.php
+++ main/database/migrations/2025_04_14_222944_create_lens_prescriptions_1_table.php
@@ -1,20 +1,40 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
-        // Create the lens_prescriptions_1 table like lens_prescriptions
-        DB::statement('CREATE TABLE lens_prescriptions_1 LIKE lens_prescriptions');
+        Schema::create('lens_prescriptions_1', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('power_id')->constrained('lens_powers_1')->onDelete('cascade');
+            $table->foreignId('type_id')->constrained()->onDelete('cascade');
+            $table->foreignId('material_id')->constrained()->onDelete('cascade');
+
+            $table->string('index');
+            $table->string('tint');
+            $table->string('diameter');
+            $table->string('focal_height');
+
+            $table->timestamps();
+        });
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
-        // Drop the lens_prescriptions_1 table if rolling back the migration
         Schema::dropIfExists('lens_prescriptions_1');
     }
 };
